d2c9297ece0ab497505adae87675056d659c1bd5
Rohit Munde <rohitvmunde@gmail.com>
2026-08-10 23:57:46 +0530

    Implemented React native BackHandler for Profile Page

--- OrdersHubMobile/src/features/home/components/profile/ProfilePage.tsx
+++ OrdersHubMobile/src/features/home/components/profile/ProfilePage.tsx
@@ -1,4 +1,5 @@
-import { View } from "react-native"
+import { useEffect } from "react";
+import { BackHandler, Text, View } from "react-native"
 
 interface ProfilePageProps {
     onBack: () => void;
@@ -6,5 +7,25 @@ interface ProfilePageProps {
 
 export const ProfilePage = (props: ProfilePageProps) => {
 
-    return <View></View>
+    useEffect(() => {
+        const backAction = (): boolean => {
+            props.onBack();
+
+            return true;
+        }
+
+        const backHandler = BackHandler.addEventListener(
+            "hardwareBackPress",
+            backAction
+        );
+
+        return () => backHandler.remove();
+    }, [props.onBack]);
+
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text style={{ fontSize: 20 }}>Profile Screen</Text>
+            {/* No manual back button needed now! */}
+        </View>
+    );
 }
